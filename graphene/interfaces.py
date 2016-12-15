401dd0cf3336b71cfbf86c5765ec789c0669641a
Danny Swaby <danny@swa.by>
2016-12-15 04:07:49 -0600

    added graphene for managing graphql server

---
+++ graphene/interfaces.py
@@ -0,0 +1,35 @@
+import graphene
+
+class Character(graphene.Interface):
+    name = graphene.String()
+
+# Human is a Character implementation
+class Human(graphene.ObjectType):
+    class Meta:
+        interfaces = (Character, )
+
+    born_in = graphene.String()
+
+# Droid is a Character implementation
+class Droid(graphene.ObjectType):
+    class Meta:
+        interfaces = (Character, )
+
+    function = graphene.String()
+
+
+
+ # Example schema
+#  interface Character {
+#   name: String
+# }
+
+# type Droid implements Character {
+#   name: String
+#   function: String
+# }
+
+# type Human implements Character {
+#   name: String
+#   bornIn: String
+# }
